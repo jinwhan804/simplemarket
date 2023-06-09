ae35c157de4c1b91c32eb74e965a77412cf3703a
jisub12 <jisub11@nate.com>
2023-06-09 12:04:30 +0900

    어드민 관리 페이지 어드민만 보이게

--- BackEnd/controllers/logoutController.js
+++ BackEnd/controllers/logoutController.js
@@ -1,21 +1,5 @@
 const { User } = require("../models");
 
-// exports.logout = (req, res) => {
-//     req.session.destroy(function(){ 
-//         res.redirect('http://127.0.0.1:5500/frontEnd/login.html');
-//     });
-// };
-// exports.logout = (req, res) => {
-//     const { session } = req.session
-//     session.destroy(function(err){ 
-//         if(err){
-//             console.log(err);
-//         }
-//         else {
-//             res.redirect('http://127.0.0.1:5500/frontEnd/login.html');
-//         }
-//     });
-// };
 exports.logout = (req, res) => {
     req.session.destroy(function(err){ 
         if(err){
