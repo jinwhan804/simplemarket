7d7e1934016a746a23432c028a50007563bd34e3
jinwhan804 <jinwhan804@hanmail.net>
2023-06-13 09:40:16 +0900

    배포 test

--- FrontEnd/app.js
+++ FrontEnd/app.js
@@ -8,6 +8,46 @@ app.get('/',(req,res)=>{
    res.send(page);
 })
 
+app.get('/login',(req,res)=>{
+   const page = fs.readFileSync("./login.html","utf-8");
+   res.send(page);
+})
+
+app.get('/mypage',(req,res)=>{
+   const page = fs.readFileSync("./mypage.html","utf-8");
+   res.send(page);
+})
+
+app.get('/signUp',(req,res)=>{
+   const page = fs.readFileSync("./signUp.html","utf-8");
+   res.send(page);
+})
+
+app.get('/signUpList',(req,res)=>{
+   const page = fs.readFileSync("./signUpList.html","utf-8");
+   res.send(page);
+})
+
+app.get('/post',(req,res)=>{
+   const page = fs.readFileSync("./post.html","utf-8");
+   res.send(page);
+})
+
+app.get('/insert',(req,res)=>{
+   const page = fs.readFileSync("./insert.html","utf-8");
+   res.send(page);
+})
+
+app.get('/detail',(req,res)=>{
+   const page = fs.readFileSync("./detail.html","utf-8");
+   res.send(page);
+})
+
+app.get('/update',(req,res)=>{
+   const page = fs.readFileSync("./update.html","utf-8");
+   res.send(page);
+})
+
 app.listen(3000,()=>{
    console.log('front server on');
 })
