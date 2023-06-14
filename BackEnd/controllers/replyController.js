23d657764040f751f5673e4df154a448ded48be8
jinwhan804 <jinwhan804@hanmail.net>
2023-06-14 16:21:46 +0900

    0614 작업

--- BackEnd/controllers/replyController.js
+++ BackEnd/controllers/replyController.js
@@ -1,16 +1,56 @@
-const {Reply} =require('../models');
+const {Reply,User} =require('../models');
 
 exports.ReplyViewAll = async(req,res)=>{
     try {
         const id = req.body.data;
         const reply = await Reply.findAll({
+            where : {postId : id},
             include : {
-                model : Post,
-                where : {id}
+                model : User
             }
         })
 
         res.json(reply);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+exports.ReplyInsert = async(req,res)=>{
+    try {
+        const {content, postId, userId} = req.body;
+
+        await Reply.create({
+            content,
+            postId,
+            userId
+        })
+
+        res.send(`${process.env.FRONT}/detail${process.env.END}`);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+exports.ReplyUpdate = async(req,res)=>{
+    try {
+        const {content, id} = req.body;
+    
+        await Reply.update({content},{where : {id}})        
+
+        res.send(`${process.env.FRONT}/detail${process.env.END}`);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+exports.ReplyDelete = async(req,res)=>{
+    try {
+        const id = req.body.data;
+    
+        await Reply.destroy({where : {id}})
+
+        res.send(`${process.env.FRONT}/detail${process.env.END}`);        
     } catch (error) {
         console.log(error);
     }
--- BackEnd/routers/reply.js
+++ BackEnd/routers/reply.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
 
-const {ReplyViewAll} = require('../controllers/replyController');
+const {ReplyViewAll,ReplyInsert,ReplyDelete,ReplyUpdate} = require('../controllers/replyController');
 
 router.post('/',ReplyViewAll);
+
+router.post('/insert',ReplyInsert);
+
+router.post('/update',ReplyUpdate);
+
+router.post('/delete',ReplyDelete);
 
 module.exports = router;
