61555ddb008e3edfbf1af4d53338cbd24ef877f4
jinwhan804 <jinwhan804@hanmail.net>
2023-06-14 16:55:24 +0900

    0614 작업

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,8 +1,8 @@
 // 로컬 주소 (확인할 것)
-const serverUrl = 'http://127.0.0.1:8080'
+// const serverUrl = 'http://127.0.0.1:8080'
 
 // // 인스턴스 탄력적 IP
-// const serverUrl = '3.35.211.37'
+const serverUrl = '3.35.211.37'
 
 const API = axios.create({
     baseURL : serverUrl,
