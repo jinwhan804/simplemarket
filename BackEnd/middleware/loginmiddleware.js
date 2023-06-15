ba100183ca05262f02f7eebf3a97deaa7c686ce0
jinwhan804 <jinwhan804@hanmail.net>
2023-06-15 16:39:36 +0900

    aa

--- BackEnd/middleware/loginmiddleware.js
+++ BackEnd/middleware/loginmiddleware.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken");
 
 exports.isLogin = (req, res, next) => {
     const { access_token } = req.session;
+    console.log(req);
     // console.log(access_token);
     jwt.verify(access_token, process.env.ACCESS_TOKEN_KEY, (err, acc_decoded) => {
         if (err) {
