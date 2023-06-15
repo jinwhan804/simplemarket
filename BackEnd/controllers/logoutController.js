5774a9ca2bd5c58ccb196c98c51694c1ff52c246
jinwhan804 <jinwhan804@hanmail.net>
2023-06-15 17:37:12 +0900

    aa

--- BackEnd/controllers/logoutController.js
+++ BackEnd/controllers/logoutController.js
@@ -7,7 +7,11 @@ exports.logout = (req, res) => {
         }
         else {
             const token = '';
-            req.session.access_token = token;
+            for (const key in req.sessionStore.sessions) {
+                let json = JSON.parse(`${req.sessionStore.sessions[key]}`);
+                json.access_token = token;                
+            }
+            console.log(req.sessionStore.sessions);
             
             res.clearCookie('connect.sid');
             res.send("메인 페이지");
