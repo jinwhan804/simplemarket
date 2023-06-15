6a4a4f56651384b6b845d249b4d86a7cc7fb7628
jinwhan804 <jinwhan804@hanmail.net>
2023-06-15 09:42:03 +0900

    adfawer

--- BackEnd/config/index.js
+++ BackEnd/config/index.js
@@ -3,7 +3,7 @@ const config = {
         username : process.env.DATABASE_USERNAME,
         password : process.env.DATABASE_PASSWORD,
         database : process.env.DATABASE_NAME,
-        host : process.env.HOST,
+        host : process.env.DATABASE_HOST,
         dialect : "mysql",
         timezone : '+09:00',
         dialectOptions : {
