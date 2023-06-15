8ec3dc6ed806a4adc666657322521215273edde2
jinwhan804 <jinwhan804@hanmail.net>
2023-06-15 16:58:55 +0900

    aa

--- BackEnd/middleware/loginmiddleware.js
+++ BackEnd/middleware/loginmiddleware.js
@@ -4,6 +4,7 @@ exports.isLogin = (req, res, next) => {
     let access_token;
     for (const key in req.sessionStore.sessions) {
         access_token = req.sessionStore.sessions[key].access_token
+        console.log(req.sessionStore.sessions[key]);
     }
     console.log("aaaaaaaaaaaaaaa",access_token);
     jwt.verify(access_token, process.env.ACCESS_TOKEN_KEY, (err, acc_decoded) => {
