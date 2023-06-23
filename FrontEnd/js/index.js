c22dfb759cda74d32f1399c1e13c806853d9af55
1chori <pro4456@naver.com>
2023-06-23 19:18:22 +0900

    dfdaf

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -1,12 +1,12 @@
 // 로컬 주소 (확인할 것)
-// const serverUrl = 'http://127.0.0.1:8080';
-// const urlEnd = '.html';
-// const mainUrl = 'main.html';
+const serverUrl = 'http://127.0.0.1:3030';
+const urlEnd = '.html';
+const mainUrl = 'main.html';
 
 // 인스턴스 탄력적 IP
-const serverUrl = 'http://3.35.211.37';
-const urlEnd = '';
-const mainUrl = '';
+// const serverUrl = 'http://3.35.211.37';
+// const urlEnd = '';
+// const mainUrl = '';
 
 const API = axios.create({
     baseURL: serverUrl,
